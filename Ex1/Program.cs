0309376de97e50ebf1d7cdd6c3d18933faff1448
rickspfc <ricardo.111campos@gmail.com>
2024-09-13 11:36:25 -0300

    Add project files.

---
+++ Ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex1;
+
+class program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Digite seu primeiro nome: ");
+        string nome = Console.ReadLine();
+
+        Console.WriteLine("Olá, " +nome+ "! Seja muito bem-vindo!");
+    }
+}
